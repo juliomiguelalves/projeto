8efd3a4b2c84fbd6947d71a2d164da7b7effdb36
Júlio Alves <59064168+juliomiguelalves@users.noreply.github.com>
2022-06-13 16:53:28 +0100

    botão download para cima

--- auth-server/app.js
+++ auth-server/app.js
@@ -86,8 +86,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+
+
 app.use(passport.initialize());
 app.use(passport.session());
+
+
 
 app.use('/users', usersRouter);
 
